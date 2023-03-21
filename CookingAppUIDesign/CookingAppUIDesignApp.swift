8a871e8db4614e8f53e3cc39b3f2a3d85f717aec
Ahmed Ali <aali.97@yahoo.com>
2023-03-21 12:58:16 +0500

    Initial

---
+++ CookingAppUIDesign/CookingAppUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppUIDesignApp.swift
+//  CookingAppUIDesign
+//
+//  Created by Ahmed Ali on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookingAppUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
